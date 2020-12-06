f996ccc465c49965b9dfad29f574f1798378765b
XYROC <xiroc.minecraft@gmail.com>
2020-12-06 13:37:58 +0100

    added a proper bounding box check for the floor decorations
    Signed-off-by: XYROC <xiroc.minecraft@gmail.com>

--- src/main/java/xiroc/dungeoncrawl/dungeon/decoration/FloorDecoration.java
+++ src/main/java/xiroc/dungeoncrawl/dungeon/decoration/FloorDecoration.java
@@ -44,13 +44,12 @@ public class FloorDecoration implements IDungeonDecoration {
         for (int x = 0; x < model.width; x++) {
             for (int z = 0; z < model.length; z++) {
                 for (int y = 0; y < model.height; y++) {
+                    BlockPos blockPos = IDungeonDecoration.getRotatedBlockPos(x, y + 1, z, pos, model, piece.rotation);
                     if (model.model[x][y][z] != null && model.model[x][y][z].type == DungeonModelBlockType.FLOOR && y < model.height - 1
-                            && world.chunkExists(pos.getX() >> 4, pos.getZ() >> 4)
-                            && world.isAirBlock(new BlockPos(pos.getX() + x, pos.getY() + y + 1, pos.getZ() + z))
+                            && worldGenBounds.isVecInside(blockPos) && structureBounds.isVecInside(blockPos) && world.isAirBlock(new BlockPos(pos.getX() + x, pos.getY() + y + 1, pos.getZ() + z))
                             && checkSolid(world, new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z), worldGenBounds, structureBounds)
                             && WeightedRandomBlock.RANDOM.nextFloat() < chance) {
-                        piece.setBlockState(blockStateProvider.get(), world, worldGenBounds, null,
-                                IDungeonDecoration.getRotatedBlockPos(x, y + 1, z, pos, model, piece.rotation),
+                        piece.setBlockState(blockStateProvider.get(), world, worldGenBounds, null, blockPos,
                                 0, 0, PlacementBehaviour.SOLID);
                     }
                 }
@@ -79,25 +78,21 @@ public class FloorDecoration implements IDungeonDecoration {
                 for (int z = 0; z < model.length; z++) {
                     for (int y = 0; y < model.height; y++) {
                         if (model.model[x][y][z] != null && model.model[x][y][z].type == DungeonModelBlockType.FLOOR && y < model.height - 1) {
-
                             BlockPos p = new BlockPos(pos.getX() + x, pos.getY() + y + 1, pos.getZ() + z);
-                            if (world.chunkExists(p.getX() >> 4, p.getZ() >> 4)) {
-                                if (world.isAirBlock(p) && world.getBlockState(p.down()).isSolid() &&
-                                        (checkSolid(world, p.north(), worldGenBounds, structureBounds) || checkSolid(world, p.east(), worldGenBounds, structureBounds)
-                                                || checkSolid(world, p.south(), worldGenBounds, structureBounds) || checkSolid(world, p.west(), worldGenBounds, structureBounds))
-                                        && WeightedRandomBlock.RANDOM.nextFloat() < chance) {
-                                    piece.setBlockState(blockStateProvider.get(), world, worldGenBounds, null,
-                                            IDungeonDecoration.getRotatedBlockPos(x, y + 1, z, pos, model, piece.rotation),
-                                            0, 0, PlacementBehaviour.SOLID);
-                                }
-                            }
+                            BlockPos blockPos = IDungeonDecoration.getRotatedBlockPos(x, y + 1, z, pos, model, piece.rotation);
 
+                            if (worldGenBounds.isVecInside(blockPos) && structureBounds.isVecInside(blockPos) && world.isAirBlock(p) && world.getBlockState(p.down()).isSolid() &&
+                                    (checkSolid(world, p.north(), worldGenBounds, structureBounds) || checkSolid(world, p.east(), worldGenBounds, structureBounds)
+                                            || checkSolid(world, p.south(), worldGenBounds, structureBounds) || checkSolid(world, p.west(), worldGenBounds, structureBounds))
+                                    && WeightedRandomBlock.RANDOM.nextFloat() < chance) {
+                                piece.setBlockState(blockStateProvider.get(), world, worldGenBounds, null, blockPos, 0, 0, PlacementBehaviour.SOLID);
+
+                            }
                         }
                     }
                 }
             }
         }
-
 
     }
 }

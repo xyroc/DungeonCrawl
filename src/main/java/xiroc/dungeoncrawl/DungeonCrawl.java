6e4e416c605930f06a26ef16be1e4623a2053ee3
xyroc <xiroc.minecraft@gmail.com>
2021-08-04 20:46:35 +0200

    Bump version to 2.3.1

--- src/main/java/xiroc/dungeoncrawl/DungeonCrawl.java
+++ src/main/java/xiroc/dungeoncrawl/DungeonCrawl.java
@@ -55,7 +55,7 @@ public class DungeonCrawl {
 
     public static final String MOD_ID = "dungeoncrawl";
     public static final String NAME = "Dungeon Crawl";
-    public static final String VERSION = "2.3.0";
+    public static final String VERSION = "2.3.1";
 
     public static final Logger LOGGER = LogManager.getLogger(NAME);
 

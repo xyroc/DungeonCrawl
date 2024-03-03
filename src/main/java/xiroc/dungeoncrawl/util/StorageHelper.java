ceb63ebf0f579093f6fa68a3ad456af11492dbd9
xyroc <xiroc.minecraft@gmail.com>
2024-03-04 00:44:31 +0100

    add a Block Codec

--- src/main/java/xiroc/dungeoncrawl/util/StorageHelper.java
+++ src/main/java/xiroc/dungeoncrawl/util/StorageHelper.java
@@ -3,12 +3,16 @@ package xiroc.dungeoncrawl.util;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
 import java.util.List;
 
 public interface StorageHelper {
+    Codec<Block> BLOCK_CODEC = ResourceLocation.CODEC.xmap(Registry.BLOCK::get, Registry.BLOCK::getKey);
     Codec<List<BlockPos>> BLOCK_POS_LIST_CODEC = Codec.list(BlockPos.CODEC);
 
     static <T> Tag encode(T value, Codec<T> codec) {

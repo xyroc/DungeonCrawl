9863845ef9d6bfeb85331b755156e9cd65ff1b69
xyroc <xiroc.minecraft@gmail.com>
2024-06-13 15:29:11 +0200

    refactor the parsing functions for datapack registries

--- src/main/java/xiroc/dungeoncrawl/datapack/DatapackRegistry.java
+++ src/main/java/xiroc/dungeoncrawl/datapack/DatapackRegistry.java
@@ -3,6 +3,7 @@ package xiroc.dungeoncrawl.datapack;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
+import xiroc.dungeoncrawl.datapack.delegate.Delegate;
 import xiroc.dungeoncrawl.exception.DatapackLoadException;
 
 import java.io.InputStreamReader;
@@ -17,18 +18,18 @@ public class DatapackRegistry<T> {
 
     private final DatapackDirectories.Directory directory;
     private final Consumer<BiConsumer<ResourceLocation, T>> builtin;
-    private final BiFunction<ResourceLocation, Reader, T> fromJson;
+    private final Parser<T> parser;
 
     private ImmutableMap<ResourceLocation, T> values;
 
     public DatapackRegistry(DatapackDirectories.Directory directory, Consumer<BiConsumer<ResourceLocation, T>> builtin, Function<Reader, T> fromJson) {
-        this(directory, builtin, (key, reader) -> fromJson.apply(reader));
+        this(directory, builtin, Parser.simple(fromJson));
     }
 
-    public DatapackRegistry(DatapackDirectories.Directory directory, Consumer<BiConsumer<ResourceLocation, T>> builtin, BiFunction<ResourceLocation, Reader, T> fromJson) {
+    public DatapackRegistry(DatapackDirectories.Directory directory, Consumer<BiConsumer<ResourceLocation, T>> builtin, Parser<T> parser) {
         this.directory = directory;
         this.builtin = builtin;
-        this.fromJson = fromJson;
+        this.parser = parser;
     }
 
     public void reload(ResourceManager resourceManager) {
@@ -37,7 +38,7 @@ public class DatapackRegistry<T> {
         resourceManager.listResources(directory.path(), path -> path.endsWith(FILE_ENDING)).forEach(resource -> {
             try {
                 ResourceLocation key = directory.key(resource, FILE_ENDING);
-                T value = fromJson.apply(key, new InputStreamReader(resourceManager.getResource(resource).getInputStream()));
+                T value = parser.parse(resourceManager, key, new InputStreamReader(resourceManager.getResource(resource).getInputStream()));
                 builder.put(key, value);
             } catch (Exception exception) {
                 throw new DatapackLoadException("Failed to load " + resource.toString() + ": " + exception.getMessage());
@@ -48,5 +49,25 @@ public class DatapackRegistry<T> {
 
     public T get(ResourceLocation key) {
         return values.get(key);
+    }
+
+    public Delegate<T> delegateOrThrow(ResourceLocation key) {
+        T value = get(key);
+        if (value == null) {
+            throw new RuntimeException("No value for key " + key);
+        }
+        return Delegate.of(value, key);
+    }
+
+    public interface Parser<T> {
+        static <T> Parser<T> simple(Function<Reader, T> parser) {
+            return keyed((key, reader) -> parser.apply(reader));
+        }
+
+        static <T> Parser<T> keyed(BiFunction<ResourceLocation, Reader, T> parser) {
+            return (resourceManager, key, reader) -> parser.apply(key, reader);
+        }
+
+        T parse(ResourceManager resourceManager, ResourceLocation key, Reader reader) throws Exception;
     }
 }

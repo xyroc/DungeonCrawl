e54754ad189218cc2f538027a7b9106d62c52bc2
xyroc <xiroc.minecraft@gmail.com>
2024-11-01 03:05:27 +0100

    update builder for random mappings for improved inheritance behavior and friendlier errors

--- src/main/java/xiroc/dungeoncrawl/util/random/RandomMapping.java
+++ src/main/java/xiroc/dungeoncrawl/util/random/RandomMapping.java
@@ -8,7 +8,9 @@ import xiroc.dungeoncrawl.datapack.registry.Delegate;
 import xiroc.dungeoncrawl.datapack.registry.InheritingBuilder;
 import xiroc.dungeoncrawl.exception.DatapackLoadException;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Random;
 import java.util.function.Function;
 
@@ -20,17 +22,18 @@ public class RandomMapping<K, V> {
     private final ImmutableMap<K, IRandom<Delegate<V>>> entries;
 
     private RandomMapping(Builder<K, V> builder) {
+        Objects.requireNonNull(builder.fallback, "No fallback pool was provided");
         try {
             this.fallback = builder.fallback.build();
         } catch (Exception e) {
-            throw new DatapackLoadException("Invalid fallback: " + e.getMessage());
+            throw new DatapackLoadException("Invalid fallback pool: " + e.getMessage());
         }
         ImmutableMap.Builder<K, IRandom<Delegate<V>>> entries = ImmutableMap.builder();
         builder.entries.forEach((key, value) -> {
             try {
                 entries.put(key, value.build());
             } catch (Exception e) {
-                throw new DatapackLoadException("Invalid entry with key '" + key + "': " + e.getMessage());
+                throw new DatapackLoadException("Invalid pool for key '" + key + "': " + e.getMessage());
             }
         });
         this.entries = entries.build();
@@ -53,7 +56,8 @@ public class RandomMapping<K, V> {
     }
 
     public static class Builder<K, V> extends InheritingBuilder<RandomMapping<K, V>, Builder<K, V>> {
-        private IRandom.Builder<Delegate<V>> fallback = new IRandom.Builder<>();
+        @Nullable
+        private IRandom.Builder<Delegate<V>> fallback = null;
         private HashMap<K, IRandom.Builder<Delegate<V>>> entries = new HashMap<>();
 
         private IRandom.Builder<Delegate<V>> get(K key) {
@@ -65,8 +69,8 @@ public class RandomMapping<K, V> {
             return this;
         }
 
-        public Builder<K, V> fallback(ResourceLocation value, int weight) {
-            this.fallback.add(Delegate.of(value), weight);
+        public Builder<K, V> fallback(IRandom.Builder<Delegate<V>> fallback) {
+            this.fallback = fallback;
             return this;
         }
 
@@ -87,7 +91,7 @@ public class RandomMapping<K, V> {
 
         @Override
         public Builder<K, V> inherit(Builder<K, V> from) {
-            this.fallback = inheritOrReplace(this.fallback, from.fallback);
+            this.fallback = InheritingBuilder.inheritOrReplaceOrChoose(this.fallback, from.fallback);
             if (from.replace()) {
                 this.entries = from.entries;
             } else {

695e9db894f1c624e6e9b4d9a1f115010949fb5b
xyroc <xiroc.minecraft@gmail.com>
2025-02-04 00:53:33 +0100

    introduce TieredResource to handle mapping tier levels (such as dungeon stage) to resources (such as loot tables)

---
+++ src/main/java/xiroc/dungeoncrawl/dungeon/tier/TieredResource.java
@@ -0,0 +1,137 @@
+package xiroc.dungeoncrawl.dungeon.tier;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Represents an n to 1 mapping from non-negative integers to objects of any type.
+ * <p>
+ * The mapping is defined through tiers.
+ * For any given non-negative integer <b>n</b>, the mapping process is done by finding the tier
+ * with the highest starting point less than or equal to <b>n</b>.
+ * <p>
+ * There must always be a tier starting at the number zero to ensure that there is a mapping for all non-negative integers.
+ */
+public interface TieredResource<T> {
+
+    /**
+     * Find the resource associated with the specified tier.
+     *
+     * @param tier The tier to find the resource for. Must not be negative.
+     */
+    T forTier(int tier);
+
+    record SingleTier<T>(T resource) implements TieredResource<T> {
+        @Override
+        public T forTier(int tier) {
+            return resource;
+        }
+    }
+
+    record MultiTier<T>(T firstTier, ImmutableList<TieredResource.Tier<T>> followingTiers) implements TieredResource<T> {
+        @Override
+        public T forTier(int tier) {
+            T result = firstTier;
+            for (TieredResource.Tier<T> followingTier : followingTiers) {
+                if (followingTier.startingFrom <= tier) {
+                    result = followingTier.resource;
+                } else {
+                    break;
+                }
+            }
+            return result;
+        }
+
+    }
+
+    record Tier<T>(T resource, int startingFrom) {}
+
+    class Builder<T> {
+        private final T firstTier;
+        private final List<Tier<T>> followingTiers = new ArrayList<>();
+
+        public Builder(T firstTier) {
+            this.firstTier = firstTier;
+        }
+
+        public Builder<T> tier(T resource, int startingFrom) {
+            followingTiers.add(new Tier<>(resource, startingFrom));
+            return this;
+        }
+
+        public TieredResource<T> build() {
+            if (followingTiers.isEmpty()) {
+                return new SingleTier<>(firstTier);
+            }
+            sortTiers();
+            return new MultiTier<>(firstTier, ImmutableList.copyOf(followingTiers));
+        }
+
+        private void sortTiers() {
+            followingTiers.sort(Comparator.comparingInt(Tier::startingFrom));
+        }
+    }
+
+    class BuilderSerializer<T> implements JsonSerializer<Builder<T>>, JsonDeserializer<Builder<T>> {
+
+        private static final String TIER_PREFIX = "tier_";
+
+        private final Type resourceType;
+
+        public BuilderSerializer(Type resourceType) {
+            this.resourceType = resourceType;
+        }
+
+        @Override
+        public Builder<T> deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+            final JsonObject jsonObject = json.getAsJsonObject();
+
+            final String firstTierKey = TIER_PREFIX + '0';
+            if (!jsonObject.has(firstTierKey)) {
+                throw new JsonParseException("tier_0 is missing");
+            }
+            final T firstTier = context.deserialize(jsonObject.remove(firstTierKey), resourceType);
+
+            final Builder<T> builder = new Builder<>(firstTier);
+            jsonObject.entrySet().forEach(entry -> {
+                final String key = entry.getKey();
+                if (!key.startsWith(TIER_PREFIX)) {
+                    throw new JsonParseException("Invalid key: " + key + " does not start with " + TIER_PREFIX);
+                }
+                final String tierString = key.substring(TIER_PREFIX.length());
+                try {
+                    final int tier = Integer.parseUnsignedInt(tierString);
+                    builder.tier(context.deserialize(entry.getValue(), resourceType), tier);
+                } catch (NumberFormatException e) {
+                    throw new JsonParseException("Invalid tier: " + tierString + " is not a non-negative integer");
+                }
+            });
+
+            return builder;
+        }
+
+        @Override
+        public JsonElement serialize(Builder<T> builder, Type type, JsonSerializationContext context) {
+            builder.sortTiers();
+
+            final JsonObject object = new JsonObject();
+            object.add(TIER_PREFIX + '0', context.serialize(builder.firstTier, resourceType));
+            for (Tier<T> followingTier : builder.followingTiers) {
+                object.add(TIER_PREFIX + followingTier.startingFrom, context.serialize(followingTier.resource, resourceType));
+            }
+            return object;
+        }
+
+    }
+}
---
+++ src/test/java/com/github/xyroc/dungeoncrawl/dungeon/tier/TieredResourceTest.java
@@ -0,0 +1,117 @@
+package com.github.xyroc.dungeoncrawl.dungeon.tier;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import xiroc.dungeoncrawl.dungeon.tier.TieredResource;
+
+import java.lang.reflect.Type;
+
+import static org.assertj.core.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class TieredResourceTest {
+
+    private static final Type TIERED_STRING_TYPE = TypeToken.getParameterized(TieredResource.class, String.class).getType();
+
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(TIERED_STRING_TYPE, new TieredResource.BuilderSerializer<String>(String.class))
+            .create();
+
+    @Test
+    void serializesSingleTierBuilderCorrectly() {
+        final var builder = new TieredResource.Builder<>("first tier");
+        final JsonElement json = GSON.toJsonTree(builder, TIERED_STRING_TYPE);
+
+        assertThat(json).isNotNull();
+        assertThat(json.isJsonObject()).isTrue();
+        assertThat(json.getAsJsonObject().has("tier_0")).isTrue();
+        assertThat(json.getAsJsonObject().get("tier_0").getAsString()).isEqualTo("first tier");
+
+        final var deserializedBuilder = GSON.fromJson(json, TIERED_STRING_TYPE);
+        assertThat(deserializedBuilder).isNotNull();
+        assertThat(deserializedBuilder).usingRecursiveComparison().isEqualTo(builder);
+    }
+
+    @Test
+    void serializesMultiTierBuilderCorrectly() {
+        final var builder = new TieredResource.Builder<>("first tier")
+                .tier("second tier", 2)
+                .tier("third tier", 5);
+        final JsonElement json = GSON.toJsonTree(builder, TIERED_STRING_TYPE);
+
+        assertThat(json).isNotNull();
+        assertThat(json.isJsonObject()).isTrue();
+        assertThat(json.getAsJsonObject().has("tier_0")).isTrue();
+        assertThat(json.getAsJsonObject().get("tier_0").getAsString()).isEqualTo("first tier");
+        assertThat(json.getAsJsonObject().has("tier_2")).isTrue();
+        assertThat(json.getAsJsonObject().get("tier_2").getAsString()).isEqualTo("second tier");
+        assertThat(json.getAsJsonObject().has("tier_5")).isTrue();
+        assertThat(json.getAsJsonObject().get("tier_5").getAsString()).isEqualTo("third tier");
+
+        final var deserializedBuilder = GSON.fromJson(json, TIERED_STRING_TYPE);
+        assertThat(deserializedBuilder).isNotNull();
+        assertThat(deserializedBuilder).usingRecursiveComparison().isEqualTo(builder);
+    }
+
+    @Test
+    void deserializeThrowsWhenMissingTierZero() {
+        final JsonObject json = new JsonObject();
+        json.addProperty("tier_1", "second tier");
+        json.addProperty("tier_4", "third tier");
+
+        assertThatThrownBy(() -> GSON.fromJson(json, TIERED_STRING_TYPE)).isInstanceOf(JsonParseException.class);
+    }
+
+    @Test
+    void buildsSingleTierResource() {
+        final var builder = new TieredResource.Builder<>(new Object());
+        final var tieredResource = builder.build();
+
+        assertThat(tieredResource).isNotNull();
+        assertThat(tieredResource).isInstanceOf(TieredResource.SingleTier.class);
+    }
+
+    @Test
+    void buildsMultiTierResource() {
+        final var builder = new TieredResource.Builder<>(new Object())
+                .tier(new Object(), 2);
+        final var tieredResource = builder.build();
+
+        assertThat(tieredResource).isNotNull();
+        assertThat(tieredResource).isInstanceOf(TieredResource.MultiTier.class);
+    }
+
+    @Test
+    void singleTierReturnsCorrectResource() {
+        final String resource = "resource";
+        final var tieredResource = new TieredResource.SingleTier<>(resource);
+
+        assertThat(tieredResource.forTier(0)).isEqualTo(resource);
+        assertThat(tieredResource.forTier(1)).isEqualTo(resource);
+    }
+
+    @Test
+    void multiTierReturnsCorrectResource() {
+        final var tieredResource = new TieredResource.MultiTier<>("first tier", ImmutableList.of(
+                new TieredResource.Tier<>("second tier", 3),
+                new TieredResource.Tier<>("third tier", 5))
+        );
+
+        assertThat(tieredResource.forTier(0)).isEqualTo("first tier");
+        assertThat(tieredResource.forTier(1)).isEqualTo("first tier");
+        assertThat(tieredResource.forTier(2)).isEqualTo("first tier");
+        assertThat(tieredResource.forTier(3)).isEqualTo("second tier");
+        assertThat(tieredResource.forTier(4)).isEqualTo("second tier");
+        assertThat(tieredResource.forTier(5)).isEqualTo("third tier");
+        assertThat(tieredResource.forTier(Integer.MAX_VALUE)).isEqualTo("third tier");
+    }
+
+}

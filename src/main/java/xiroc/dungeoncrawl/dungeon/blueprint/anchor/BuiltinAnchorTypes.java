e13dd2a27f12e7c44d1068c071de78d6440cf4c5
xyroc <xiroc.minecraft@gmail.com>
2025-02-24 16:42:32 +0100

    remove unused anchor types

--- src/main/java/xiroc/dungeoncrawl/dungeon/blueprint/anchor/BuiltinAnchorTypes.java
+++ src/main/java/xiroc/dungeoncrawl/dungeon/blueprint/anchor/BuiltinAnchorTypes.java
@@ -7,6 +7,4 @@ public interface BuiltinAnchorTypes {
     ResourceLocation JUNCTURE = DungeonCrawl.locate("juncture");
     ResourceLocation ENTRANCE = DungeonCrawl.locate("entrance");
     ResourceLocation STAIRCASE = DungeonCrawl.locate("staircase");
-    ResourceLocation MEGA_NODE_ENTRANCE = DungeonCrawl.locate("mega_node_entrance");
-    ResourceLocation MEGA_NODE_CONNECTOR = DungeonCrawl.locate("mega_node_connector");
 }

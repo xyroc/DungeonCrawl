f29f11ea6f85f02ae837748fc5190b241d3480d8
xyroc <xiroc.minecraft@gmail.com>
2025-05-05 17:20:25 +0200

    add javadoc for class TemplateBlock

--- src/main/java/xiroc/dungeoncrawl/dungeon/blueprint/template/block/TemplateBlock.java
+++ src/main/java/xiroc/dungeoncrawl/dungeon/blueprint/template/block/TemplateBlock.java
@@ -4,5 +4,14 @@ import net.minecraft.core.Vec3i;
 import xiroc.dungeoncrawl.dungeon.block.MetaBlock;
 import xiroc.dungeoncrawl.dungeon.blueprint.template.block.type.TemplateBlockType;
 
-public record TemplateBlock(TemplateBlockType type, Vec3i position, MetaBlock block, TemplateBlockPlacementSettings settings) {
+/**
+ * Represents a single block within a template blueprint.
+ *
+ * @param type     The type of this template block.
+ * @param position The relative position within the blueprint.
+ * @param block    The block state properties associated with this block.
+ * @param settings A set of rules defining how this block is placed during world generation.
+ */
+public record TemplateBlock(TemplateBlockType type, Vec3i position, MetaBlock block,
+                            TemplateBlockPlacementSettings settings) {
 }

610bf04b5d55fe349a9b7569c2a76a1576eff485
xyroc <xiroc.minecraft@gmail.com>
2025-12-21 19:35:51 +0100

    add tests for nested blueprint multi-parts

--- src/test/java/com/github/xyroc/dungeoncrawl/dungeon/blueprint/BlueprintMultipartTest.java
+++ src/test/java/com/github/xyroc/dungeoncrawl/dungeon/blueprint/BlueprintMultipartTest.java
@@ -16,6 +16,7 @@ import xiroc.dungeoncrawl.datapack.registry.DatapackRegistries;
 import xiroc.dungeoncrawl.dungeon.blueprint.Blueprint;
 import xiroc.dungeoncrawl.dungeon.blueprint.BlueprintMultipart;
 import xiroc.dungeoncrawl.dungeon.blueprint.anchor.Anchor;
+import xiroc.dungeoncrawl.dungeon.blueprint.anchor.BuiltinAnchorTypes;
 import xiroc.dungeoncrawl.dungeon.component.BlueprintComponent;
 import xiroc.dungeoncrawl.dungeon.generator.level.LevelGenerator;
 import xiroc.dungeoncrawl.dungeon.generator.level.LevelGeneratorSettings;
@@ -34,7 +35,6 @@ import static org.mockito.Mockito.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class BlueprintMultipartTest {
-
     private static LevelGenerator generator;
 
     @BeforeEach
@@ -67,23 +67,32 @@ public class BlueprintMultipartTest {
         );
     }
 
+    private static void setBlueprintSize(Blueprint mock, int xSpan, int ySpan, int zSpan) {
+        when(mock.xSpan()).thenReturn(xSpan);
+        when(mock.ySpan()).thenReturn(ySpan);
+        when(mock.zSpan()).thenReturn(zSpan);
+        when(mock.boundingBox(any())).thenCallRealMethod();
+        when(mock.coordinateSpace(any())).thenCallRealMethod();
+    }
+
+    private static Holder<Blueprint> createMockBlueprintHolder(Blueprint blueprint, ResourceLocation key) {
+        Holder<Blueprint> holder = mock();
+        ResourceKey<Blueprint> selfReferringBlueprintResourceKey = ResourceKey.create(DatapackRegistries.BLUEPRINT, key);
+        when(holder.value()).thenReturn(blueprint);
+        when(holder.getKey()).thenReturn(selfReferringBlueprintResourceKey);
+        return holder;
+    }
+
     @Test
     void abortsOnSelfReference() {
-        ResourceLocation testAnchor = ResourceLocation.fromNamespaceAndPath("test", "test_anchor");
         Blueprint selfReferringBlueprint = mock(Blueprint.class);
-        when(selfReferringBlueprint.anchors()).thenReturn(Map.of(testAnchor, List.of(new Anchor(new BlockPos(0, 0, 0), Direction.NORTH))));
-        when(selfReferringBlueprint.xSpan()).thenReturn(1);
-        when(selfReferringBlueprint.ySpan()).thenReturn(1);
-        when(selfReferringBlueprint.zSpan()).thenReturn(1);
-        when(selfReferringBlueprint.boundingBox(any())).thenCallRealMethod();
-        when(selfReferringBlueprint.coordinateSpace(any())).thenCallRealMethod();
+        ResourceLocation testPlacement = ResourceLocation.fromNamespaceAndPath("test", "test_anchor");
+        when(selfReferringBlueprint.anchors()).thenReturn(Map.of(testPlacement, List.of(new Anchor(new BlockPos(0, 0, 0), Direction.NORTH))));
+        setBlueprintSize(selfReferringBlueprint, 1, 1, 1);
 
-        Holder<Blueprint> holderOfSelfReferringBlueprint = mock();
-        ResourceKey<Blueprint> selfReferringBlueprintResourceKey = ResourceKey.create(DatapackRegistries.BLUEPRINT, ResourceLocation.fromNamespaceAndPath("test", "test_blueprint"));
-        when(holderOfSelfReferringBlueprint.value()).thenReturn(selfReferringBlueprint);
-        when(holderOfSelfReferringBlueprint.getKey()).thenReturn(selfReferringBlueprintResourceKey);
+        Holder<Blueprint> holderOfSelfReferringBlueprint = createMockBlueprintHolder(selfReferringBlueprint, ResourceLocation.fromNamespaceAndPath("test", "test_blueprint"));
 
-        BlueprintMultipart multipart = new BlueprintMultipart(testAnchor, new SingleValueRandom<>(holderOfSelfReferringBlueprint));
+        BlueprintMultipart multipart = new BlueprintMultipart(testPlacement, new SingleValueRandom<>(holderOfSelfReferringBlueprint));
         when(selfReferringBlueprint.parts()).thenReturn(List.of(multipart));
 
         DungeonWorldGenContext context = mock();
@@ -91,6 +100,43 @@ public class BlueprintMultipartTest {
 
         // Populating should fail
         assertThat(multipart.addParts(piece, piece.base, generator)).isTrue();
+    }
+
+    @Test
+    void placesNestedParts() {
+        Blueprint bottomLevelBlueprint = mock(Blueprint.class);
+        setBlueprintSize(bottomLevelBlueprint, 1, 1, 1);
+        ResourceLocation bottomLevelPlacement = ResourceLocation.fromNamespaceAndPath("test", "bottom_level_placement");
+        when(bottomLevelBlueprint.anchors()).thenReturn(Map.of(
+                BuiltinAnchorTypes.JUNCTURE, List.of(new Anchor(BlockPos.ZERO, Direction.WEST))
+        ));
+        Holder<Blueprint> bottomLevelBlueprintHolder = createMockBlueprintHolder(bottomLevelBlueprint, ResourceLocation.fromNamespaceAndPath("test", "bottom_level"));
+
+        Blueprint midLevelBlueprint = mock(Blueprint.class);
+        setBlueprintSize(midLevelBlueprint, 3, 3, 3);
+        ResourceLocation midLevelPlacement = ResourceLocation.fromNamespaceAndPath("test", "mid_level_placement");
+        when(midLevelBlueprint.anchors()).thenReturn(Map.of(
+                bottomLevelPlacement, List.of(new Anchor(BlockPos.ZERO, Direction.EAST)),
+                BuiltinAnchorTypes.JUNCTURE, List.of(new Anchor(BlockPos.ZERO, Direction.WEST))
+        ));
+        Holder<Blueprint> midLevelBlueprintHolder = createMockBlueprintHolder(midLevelBlueprint, ResourceLocation.fromNamespaceAndPath("test", "mid_level"));
+        BlueprintMultipart midLevelParts = new BlueprintMultipart(bottomLevelPlacement, new SingleValueRandom<>(bottomLevelBlueprintHolder));
+        when(midLevelBlueprint.parts()).thenReturn(List.of(midLevelParts));
+
+        Blueprint topLevelBlueprint = mock(Blueprint.class);
+        setBlueprintSize(topLevelBlueprint, 5, 5, 5);
+        when(topLevelBlueprint.anchors()).thenReturn(Map.of(
+                midLevelPlacement, List.of(new Anchor(BlockPos.ZERO, Direction.EAST))
+        ));
+        BlueprintMultipart topLevelParts = new BlueprintMultipart(midLevelPlacement, new SingleValueRandom<>(midLevelBlueprintHolder));
+        when(topLevelBlueprint.parts()).thenReturn(List.of(topLevelParts));
+        Holder<Blueprint> topLevelBlueprintHolder = createMockBlueprintHolder(topLevelBlueprint, ResourceLocation.fromNamespaceAndPath("test", "top_level"));
+
+        DungeonWorldGenContext context = mock();
+        BlueprintPiece piece = new BlueprintPiece(new BlueprintComponent(topLevelBlueprintHolder, BlockPos.ZERO, Rotation.NONE), context);
+
+        // Placement should succeed.
+        assertThat(topLevelParts.addParts(piece, piece.base, generator)).isFalse();
     }
 
 }
